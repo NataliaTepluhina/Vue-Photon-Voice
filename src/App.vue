79985c0a34e93b01487896b7b9a86e9e60d2f34b
NataliaTepluhina <tarya.se@gmail.com>
2018-07-28 21:48:22 +0300

    * added color block to manage colors from speech-to-text

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,11 @@
             <v-container class="app-container white" fill-height>
                 <v-layout>
                   <v-flex xs12>
-                        <v-btn fab dark color="blue darken-4">
+                        <v-btn fab dark color="blue darken-4" @click="speechToText">
                           <v-icon dark>mic</v-icon>
                         </v-btn>
+                        <h2 class="title">Result: {{result}}</h2>
+                        <div class="color-block" :style="{backgroundColor: activeColor}"></div>
                   </v-flex>
                 </v-layout>
             </v-container>
@@ -24,7 +26,46 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data() {
+    return {
+      recognition: null,
+      result: '',
+      colors: ['red', 'magenta', 'blue', 'white', 'orange', 'green', 'yellow'],
+      activeColor: ''
+    };
+  },
+  methods: {
+    speechToText() {
+      this.recognition.start();
+      this.recognition.onresult = event => {
+        this.result = event.results[0][0].transcript.toLowerCase();
+        const color = this.colors.find(item => item === this.result);
+        if (color) {
+          this.activeColor = color;
+        }
+      };
+    }
+  },
+  mounted() {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    if (!SpeechRecognition && process.env.NODE_ENV !== 'production') {
+      throw new Error(
+        'Speech Recognition does not exist on this browser. Use Chrome or Firefox'
+      );
+    }
+    this.recognition = new SpeechRecognition();
+    this.recognition.lang = 'en-US';
+
+    this.recognition.onspeechend = () => {
+      this.recognition.stop();
+    };
+
+    this.recognition.onnomatch = event => {};
+
+    this.recognition.onerror = event => {};
+  }
 };
 </script>
 
@@ -35,5 +76,15 @@ export default {
 
 .footer-text {
   width: 100%;
+}
+
+.title {
+  padding-top: 20px;
+}
+
+.color-block {
+  width: 200px;
+  height: 200px;
+  margin-top: 20px;
 }
 </style>
